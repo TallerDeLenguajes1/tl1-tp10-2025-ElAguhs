f9989e50e0e08ee2155e06f49255b9c0f9cb36fe
“ElAguhs” <rodriguezagustin589@gmail.com>
2025-06-27 11:18:50 -0300

    clase Tarea creada

---
+++ Tareas/Tareas.cs
@@ -0,0 +1,12 @@
+namespace EspacioTareas
+{
+    public class Tarea
+    {
+        public int userId { get; set; }
+        public int id {get; set; }
+        public string title {get; set; }
+        public bool completed {get; set; }
+    }
+
+    
+}
