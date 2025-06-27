e1868d16fdc6a2887cd3db7c58f69cb8f6bf4276
“ElAguhs” <rodriguezagustin589@gmail.com>
2025-06-27 11:24:11 -0300

    terminado punto 1

--- Tareas/Program.cs
+++ Tareas/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text.Json;
+using EspacioTareas;
+
+HttpClient client = new HttpClient();
+
+string url = "https://jsonplaceholder.typicode.com/todos/";
+
+string cuerpo = await client.GetStringAsync(url);
+
+List<Tarea> tareas = JsonSerializer.Deserialize<List<Tarea>>(cuerpo);
+
+foreach (var tareaAux in tareas.Where(tareaAux => !tareaAux.completed))
+{
+    Console.WriteLine("Tarea:" + tareaAux.title + "Estado: Pendiente");
+}
+
+foreach (var tareaAux in tareas.Where(tareaAux => tareaAux.completed))
+{
+    Console.WriteLine("Tarea:" + tareaAux.title + "Estado: Realizada");
+}
+
+string archivoJson = JsonSerializer.Serialize(tareas);
+File.WriteAllText("tareas.json", archivoJson);
