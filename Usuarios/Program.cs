37ebdfd62fb9dbbc7c9c768ca2731711670db60e
“ElAguhs” <rodriguezagustin589@gmail.com>
2025-06-27 11:28:47 -0300

    mostrar por pantalla

--- Usuarios/Program.cs
+++ Usuarios/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text.Json;
+using EspacioUsuarios;
+
+HttpClient client = new HttpClient();
+
+string url = "https://jsonplaceholder.typicode.com/users";
+
+string cuerpo = await client.GetStringAsync(url);
+
+List<Usuario> usuarios = JsonSerializer.Deserialize<List<Usuario>>(cuerpo);
+
+Console.WriteLine("Primeros 5 usuarios");
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine("Usuario: " + usuarios[i].name);
+    Console.WriteLine("Correo: " + usuarios[i].email);
+    Console.WriteLine("Domicilio: "+ usuarios[i].address.city  + " "+ usuarios[i].address.street +" "+ usuarios[i].address.suite);
+}
