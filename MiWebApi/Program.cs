882f7ac259cfd0a2c205b14d9e31b1480f6be7b6
“ElAguhs” <rodriguezagustin589@gmail.com>
2025-06-27 11:32:49 -0300

    se agrego el program.cs terminado

--- MiWebApi/Program.cs
+++ MiWebApi/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net.Http;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+using EspacioComercio;
+
+HttpClient client = new HttpClient();
+string url = "https://fakestoreapi.com/products";
+
+string cuerpo = await client.GetStringAsync(url);
+
+List<Productos> producto = JsonSerializer.Deserialize<List<Productos>>(cuerpo);
+
+foreach (var productoAux in producto)
+{
+    Console.WriteLine("Nombre: " + productoAux.title);
+    Console.WriteLine("Categoria: " + productoAux.category);
+    Console.WriteLine("Precio: $" + productoAux.price);
+}
+
+string archivoJson = JsonSerializer.Serialize(producto);
+File.WriteAllText("productos.json", archivoJson);
